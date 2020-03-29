109b4d3074e8dd5427088535ae410880d57526d3
Michael Chapa <michaelchapa1@gmail.com>
2020-03-29 18:41:26 -0500

    Added fread() fxn, doesn't completely work yet but will fix it and abstract it to a subRoutine. Will create another branch to fix it.

---
+++ build/Debug/GNU-Linux/part1.o.d
@@ -0,0 +1,1 @@
+build/Debug/GNU-Linux/part1.o: part1.c
--- part1.c
+++ part1.c
@@ -4,11 +4,26 @@
 int main(int argc, char** argv) {
     FILE *pInFile, *pOutFile;
     int i, j;
+    unsigned long lBuffer;
     
+    printf("%d\n", argc);
+
     // Check if files exist
-    if(argc != 2){
+    if(argc != 3){
         fprintf(stderr, "Usage: %s <infile> <outfile>\n", argv[0]);
         exit(1);
+    }
+
+    pInFile = fopen(argv[1], "r");
+    if(pInFile == NULL){
+        fprintf(stderr, "%s either doesn't exist or is inaccessible\n", argv[1]);
+        exit(1);
+    }
+    
+    for(i = 0; i < 64; i += 8){
+        fseek(pInFile, i, SEEK_SET);
+        fread(&lBuffer, 8, 1L, pInFile); // reads 8 bytes into lBuffer
+        printf("%lu\n", lBuffer);       
     }
     
     return 0;
