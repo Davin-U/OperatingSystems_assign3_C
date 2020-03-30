d9169a867e4629c8c7deb5051a3eee8f41835330
Michael Chapa <michaelchapa1@gmail.com>
2020-03-30 00:52:39 -0500

    Generated Frame number and Frame offset. Need to create the physical address and get it into the outfile. After that I can work on part 2.

--- part1.c
+++ part1.c
@@ -4,10 +4,12 @@
 #include <sys/types.h>
 #include <fcntl.h>
 
+#define PAGE_SIZE 128
+
 int main(int argc, char** argv) {
     FILE *pInFile, *pOutFile;
-    int i, iFileSize;
-    unsigned long lAddress, lAddressM[8]; // logical memory address
+    int i, iFileSize, iPageTableM[] = {2, 4, 1, 7, 3, 5, 6};
+    int iFrameNum, iFrameOffset;
     struct stat inFileInfo;
     
     if(argc != 3){ // comand line arguments
@@ -21,17 +23,22 @@ int main(int argc, char** argv) {
         exit(1);
     }
     
-    // Get inFile's size in bytes
+    // Get inFile's size in bytes, initialize logical address array
     stat(argv[1], &inFileInfo);
     iFileSize = ((int) inFileInfo.st_size / 8);
+    unsigned long lAddressM[iFileSize]; // Logical Memory addresses
 
-    // populate logical memory address array
-    for(i = 0; i < iFileSize; i++){ // read every 8 bytes
-        fseek(pInFile, (i * 8), SEEK_SET);
-        fread(&lAddress, 8, 1L, pInFile); // reads 8 bytes into lBuffer
-        lAddressM[i] = lAddress;      
+    // populate logical address array
+    for(i = 0; i < iFileSize; i++){
+        // populate logical address array
+        fseek(pInFile, (i * 8), SEEK_SET); // every 8 bytes
+        fread(&lAddressM[i], 8, 1L, pInFile);
+
+        // generate physical address
+        iFrameNum = (int) lAddressM[i] / PAGE_SIZE;
+        iFrameOffset = (int) lAddressM[i] % PAGE_SIZE;
+        printf("%d, %d\n", iFrameNum, iFrameOffset);
     }
-    
+
     return 0;
 }
-
